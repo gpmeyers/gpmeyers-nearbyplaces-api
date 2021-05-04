5449241d599ec3f6511e236cddd172c2b25065ed
Graysen Meyers <41168696+gpmeyers@users.noreply.github.com>
2021-05-04 15:37:35 -0700

    Most functionality implemented. May need small changes.

---
+++ db.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+
+let host = process.env.host;
+let database = process.env.database;
+let port = process.env.port;
+let username = process.env.mydbusername;
+let password = process.env.password;
+
+let connectionString = `postgres://${username}:${password}@${host}:${port}/${database}`;
+
+let connection = {
+    connectionString: process.env.DATABASE_URL ? process.env.DATABASE_URL : connectionString,
+    ssl: {rejectUnauthorized: false}
+};
+
+const pool = new Pool(connection);
+
+let savePlace = (name, placelocation) => {
+    return pool.query('insert into mynearbyplaces.places(name, placelocation) values ($1, $2);', [name, placelocation])
+    .then(() => {console.log('the place was saved')});
+}
+
+let getPlaces = () => {
+    let sql = 'select name, placelocation from mynearbyplaces.places;';
+    return pool.query(sql)
+    .then(result => result.rows);
+}
+
+let saveReview = (placeName, review) => {
+    let sql = 'select id from mynearbyplaces.places where (name=$s1);';
+    let placeId = pool.query(sql, [placeName])
+    .then(result => {console.log(result)});
+}
+
+let findPlace = (name, placelocation) => {
+    let sql = 'select name, placelocation from mynearbyplaces.places where (name=$s1) and (placelocation=$placelocation);';
+    return pool.query(sql, [name, placelocation])
+    .then(result => result.rows);
+}
+
+module.exports = { savePlace, getPlaces, saveReview, findPlace }
--- index.js
+++ index.js
@@ -1,24 +1,52 @@
 // dependencies
 const express = require('express');
+const cors = require('cors');
+const db = require('./db');
 
 // create the server
 const app = express();
-const port = process.env.PORT || 4002
+const port = 8080;
 
-app.post('/place', (req, res) = {
+// use cors
+let corsOptions = {
+    origin: 'https://gpmeyers.github.io',
+    optionsSuccessStatus: 200 // For legacy browser support
+}
 
+app.use(cors(corsOptions));
+
+// parse json
+app.use(express.json());
+
+app.post('/place', (req, res) => {
+    let name = req.body.name;
+    let placelocation = req.body.placelocation;
+    db.savePlace(name, placelocation);
+
+    res.send(`The place ${name} was added successfully.`);
 });
 
 app.get('/places', (req, res) => {
-
+    db.getPlaces()
+    .then(places => res.json(places))
+    .catch(e => {console.log(e); res.status(500).send('There was an error in finding the places')});
 });
 
 app.post('review/:placeName', (req, res) => {
+    let placeName = req.params.placeName;
+    let review = req.body.review;
+    db.saveReview(placeName, review);
 
+    res.send(`The review ${review} was added successfully.`);
 });
 
 app.get('/search/:placeName/:location', (req, res) => {
+    let placeName = req.params.placeName;
+    let location = req.params.location;
 
+    db.findPlace(placeName, location)
+    .then(place => res.json(place))
+    .catch(e => {console.log(e); res.status(500).send('The was an error in finding the place')});
 });
 
 app.listen(port, () => console.log('Listening on port ' + port));
